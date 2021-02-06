ddb95513f70cacce1a21e0c0de515f93f384239d
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-02-06 17:09:37 -0300

    feat: add logs and folderName arg

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+#! /usr/bin/env node
 const path = require('path')
+const cproc = require('child_process')
 const download = require('download-git-repo')
 
 const REPO_URL = 'direct:https://codeload.github.com/gaoliveira21/react-base-config/zip/main'
+const folderName = process.argv[2] ?? 'react-app'
+const fullPath = path.resolve(__dirname, folderName)
 
-download(REPO_URL, path.resolve(__dirname, 'my-react-app'), { clone: false }, (err) => {
-  if(!err) return console.log('Success')
-  console.log(err)
+console.log('Downloading files...')
+
+download(REPO_URL, fullPath, { clone: false }, (err) => {
+  if (!err) {
+    console.log('Installing dependencies...')
+    const proc = cproc.spawn('npm', ['install', '--prefix', fullPath])
+
+    proc.stdout.on('data', (response) => console.log(response.toString()))
+
+    proc.stdout.on('close', code => console.log(`child process exited with code ${code}`))
+  } else {
+    console.error(err)
+  }
 })
